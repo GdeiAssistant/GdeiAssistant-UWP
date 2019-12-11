ec6039a03575979f2600cd87bfea83719afd7f97
Pigbibi <20649888+PeachShrubFizz@users.noreply.github.com>
2019-12-11 17:34:22 +0800

    移除多余的个人资料类属性，修正昵称拼写错误

--- GdeiAssistant-UWP/Entity/Profile.cs
+++ GdeiAssistant-UWP/Entity/Profile.cs
@@ -4,22 +4,6 @@
     {
         public string username { set; get; }
 
-        public string kickname { set; get; }
-
-        public int? gender { set; get; }
-
-        public int? genderOrientation { set; get; }
-
-        public int? faculty { set; get; }
-
-        public string major { set; get; }
-
-        public string customGenderName { set; get; }
-
-        public string region { set; get; }
-
-        public string state { set; get; }
-
-        public string city { set; get; }
+        public string nickname { set; get; }
     }
 }
